e850ddf4b1d8c00d66a7425838e0af9932773fb2
Zewen Xu <zevin9427@gmail.com>
2023-09-26 17:04:56 +0800

    Implement project plan

---
+++ planner/project_planner.go
@@ -0,0 +1,51 @@
+package planner
+
+import (
+	"miniSQL/query"
+	rm "miniSQL/record_manager"
+)
+
+//ProjectPlan 对project进行计划
+type ProjectPlan struct {
+	p      Plan
+	schema *rm.Schema
+}
+
+//NewProjectPlan 使用传入的需要筛选的字段来构造projectPlan对象
+func NewProjectPlan(p Plan, fieldList []string) *ProjectPlan {
+	projectPlan := &ProjectPlan{
+		p:      p,
+		schema: rm.NewSchema(),
+	}
+	for _, field := range fieldList {
+		projectPlan.schema.Add(field, projectPlan.p.Schema()) //在他底部表的schema结构的基础上筛选我们需要的字段进来
+	}
+	return projectPlan
+}
+
+func (p *ProjectPlan) Open() (interface{}, error) {
+	s, err := p.p.Open() //获得底层的scan对象
+	if err != nil {
+		return nil, err
+	}
+	//使用当前获得的子scan对象，和需要的表结构来进行查询
+	return query.NewProjectScan(s.(query.Scan), p.schema.Fields()), nil
+}
+
+//BlockAccessed 当前project访问的block数量和其子scan是一样的
+func (p *ProjectPlan) BlockAccessed() int {
+	return p.p.BlockAccessed()
+}
+
+//RecordsOuput 当前的project访问的record和子scan是一样的
+func (p *ProjectPlan) RecordsOuput() int {
+	return p.p.RecordsOuput()
+}
+
+func (p *ProjectPlan) DistinctValues(fldName string) int {
+	return p.p.DistinctValues(fldName)
+}
+
+func (p *ProjectPlan) Schema() rm.SchemaInterface {
+	return p.schema
+}
