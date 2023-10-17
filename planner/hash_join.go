30116e7070eee36747729761881dae354ec5846c
Zewen Xu <zevin9427@gmail.com>
2023-10-17 16:12:49 +0800

    Use Bloom filter to speed up probing in PHJ algorithm.

--- planner/hash_join.go
+++ planner/hash_join.go
@@ -1,5 +1,10 @@
 package planner
 
+import (
+	"encoding/binary"
+	"github.com/bits-and-blooms/bloom/v3"
+)
+
 //假设有两个表R，S，需要根据连接键做连接操作
 type Record struct {
 	Key  int
@@ -18,23 +23,37 @@ func partition(records []Record, numPartitions int) [][]Record {
 
 // 构建哈希表,为各个分区分别构造哈希表，以及bloom filter表，加快不存在数据的排查
 //map中的key就是当前的记录的连接key，value就是当前对应的记录,
-func buildHash(records []Record) map[int]string {
+func buildHash(records []Record) (map[int]string, *bloom.BloomFilter) {
 	hashTable := make(map[int]string)
+	filter := bloom.NewWithEstimates(1000000, 0.01)
 	for _, record := range records {
 		hashTable[record.Key] = record.Data
+		filter.Add(encoded(uint32(record.Key)))
 	}
-	return hashTable
+	return hashTable, filter
 }
 
-// 探测操作,在r分区中，使用s分区已经构造好了的哈希表进行探测
-func probe(partition []Record, hashTable map[int]string) []Record {
+func encoded(key uint32) []byte {
+	n1 := make([]byte, 4)
+	binary.BigEndian.PutUint32(n1, key)
+	return n1
+}
+
+// 探测操作,在r分区中，使用s分区已经构造好了的哈希表进行探测,把被驱动表的布隆过滤器提取上来进行检测
+func probe(partition []Record, hashTable map[int]string, bloomFilter *bloom.BloomFilter) []Record {
 	var result []Record
 	//遍历r分区的数据，r分区就是驱动表，记录一般比较小，可以进行分块读取
 	for _, record := range partition {
-		//使用s分区的哈希表进行探测
-		if data, ok := hashTable[record.Key]; ok {
-			result = append(result, Record{Key: record.Key, Data: data})
+		//使用s分区的哈希表进行探测，遍历当前分区驱动表的数据，先在被驱动表的布隆过滤器中检查数据是否存在，加速排非
+		if bloomFilter.Test(encoded(uint32(record.Key))) {
+			//当前数据如果存在了，才能在哈希表中检查，获取到相应的数据
+			//如果当前在bloom filter中查找到就验证，如果当前没有找到，直接就可以返回了
+			if data, ok := hashTable[record.Key]; ok {
+				//检查发现key相同，所以当前的数据可以被提取出来,把驱动表中需要的数据提取出来,并返回当前的组合数据
+				result = append(result, Record{Key: record.Key, Data: data + record.Data})
+			}
 		}
+
 	}
 	//获得当前分区中符合条件的记录
 	return result
@@ -42,6 +61,7 @@ func probe(partition []Record, hashTable map[int]string) []Record {
 
 // 合并结果,将不同区块的符合条件的数据进行合并
 func merge(records1, records2 []Record) []Record {
+	//不同的表中数据不同
 	var result []Record
 	for _, record1 := range records1 {
 		for _, record2 := range records2 {
--- planner/hash_join_test.go
+++ planner/hash_join_test.go
@@ -1,6 +1,10 @@
 package planner
 
-import "testing"
+import (
+	"fmt"
+	"github.com/bits-and-blooms/bloom/v3"
+	"testing"
+)
 
 func TestHashJoin(t *testing.T) {
 	// 创建关系R和S
@@ -10,6 +14,7 @@ func TestHashJoin(t *testing.T) {
 		{Key: 3, Data: "C"},
 		{Key: 4, Data: "D"},
 		{Key: 5, Data: "E"},
+		{Key: 6, Data: "E"},
 	}
 	s := []Record{
 		{Key: 1, Data: "a"},
@@ -19,27 +24,36 @@ func TestHashJoin(t *testing.T) {
 		{Key: 5, Data: "e"},
 	}
 	// 分区操作
-	numPartitions := 2
+	numPartitions := 2                         //分了两个分区
 	rPartitions := partition(r, numPartitions) //r分区的结果
 	sPartitions := partition(s, numPartitions) //s分区的结果
 
 	// 构建哈希表
 	var rHashTables []map[int]string //每个分区都构造一个哈希表
 	var sHashTables []map[int]string
-	for i := 0; i < numPartitions; i++ {
-		rHashTables = append(rHashTables, buildHash(rPartitions[i])) //给各个分区分别构建哈希表
-		sHashTables = append(sHashTables, buildHash(sPartitions[i]))
+	var rBloomFilter []*bloom.BloomFilter
+	var sBloomFilter []*bloom.BloomFilter
+	for i := 0; i < numPartitions; i++ { //给每个分区分别创建哈希表和布隆过滤器
+		rht, rbf := buildHash(rPartitions[i])
+		rHashTables = append(rHashTables, rht)   //给各个分区分别构建哈希表
+		rBloomFilter = append(rBloomFilter, rbf) //给各个分区分别构建哈希表
+
+		sht, sbf := buildHash(sPartitions[i])
+		sHashTables = append(sHashTables, sht)   //给各个分区分别构建哈希表
+		sBloomFilter = append(sBloomFilter, sbf) //给各个分区分别构建哈希表
+
 	}
 
 	// 探测操作
 	var results [][]Record //各个分区中符合条件的记录
 	for i := 0; i < numPartitions; i++ {
-		results = append(results, probe(rPartitions[i], sHashTables[i]))
+		results = append(results, probe(rPartitions[i], sHashTables[i], sBloomFilter[i]))
 	}
-
+	//result里面就是各个区块中符合要求的记录
 	// 合并结果
-	finalResult := results[0]
-	for i := 1; i < numPartitions; i++ {
-		finalResult = merge(finalResult, results[i])
+	var finalResult []Record
+	for _, re := range results {
+		finalResult = append(finalResult, re...)
 	}
+	fmt.Println("he")
 }
