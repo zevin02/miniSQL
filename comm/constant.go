ddafb319be5439e06921c627474c103cd4947d73
Zewen Xu <zevin9427@gmail.com>
2024-04-06 16:38:13 +0800

    encode Constant to hashcode

--- comm/constant.go
+++ comm/constant.go
@@ -1,6 +1,10 @@
 package comm
 
-import "strconv"
+import (
+	"hash/fnv"
+	"math/big"
+	"strconv"
+)
 
 //Constant 用户可以不用指定string或者int类型数据的插入,这个可以表示一个常量
 type Constant struct {
@@ -53,4 +57,20 @@ func (c *Constant) Equal(obj *Constant) bool {
 		return *c.Sval == *obj.Sval
 	}
 	return false
+}
+
+//HashCode 获得他的一个哈希值
+func (c *Constant) HashCode() uint32 {
+	var bytes []byte
+	h := fnv.New32a()
+	if c.Ival != nil {
+		//将数值转化成字节数组，然后再进行编码
+		s := big.NewInt(int64(*c.Ival)) //转化成一个Int类型的变量
+		bytes = s.Bytes()               //将他转化成一个字节数组
+	} else {
+		bytes = []byte(*c.Sval) //如果是字符串类型，就可以直接将他转化成一个字节数组
+	}
+	h.Write(bytes) //写入到这个对象中去
+	//根据写入的数据，生成一个哈希值
+	return h.Sum32()
 }
